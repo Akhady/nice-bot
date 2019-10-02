11086570911a12c5d57ff92499cfec56c57668c2
Akhady <melicharada@seznam.cz>
2019-10-02 12:16:45 +0200

    Initial commit.

---
+++ bot.py
@@ -0,0 +1,25 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'nice':
+        await message.channel.send("nice")
+
+client.run(TOKEN)
