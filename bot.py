24bc168503297d6c8fd3316cefbd4464fa6618b2
Jakub Man <jakubman1@gmail.com>
2019-10-02 13:34:47 +0200

    ADD "nice top" command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from operator import itemgetter
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,9 +11,13 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+data = {}
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
+    with open("nice_data.json", "w+") as data_file:
+        data = json.loads(data_file.read())
 
 @client.event
 async def on_message(message):
@@ -20,6 +25,38 @@ async def on_message(message):
         return
 
     if message.content == 'nice':
+        add_to_user(message.author)
         await message.channel.send("nice")
+    elif message.content == 'Nice':
+        add_to_user(message.author)
+        await message.channel.send("Nice")
+    elif message.content.lower() == 'nice top':
+        await message.channel.send(get_top_users())
 
 client.run(TOKEN)
+
+
+def add_to_user(username):
+    if username in data:
+        data[username].value += 1
+    else:
+        data[username] = 1
+
+def save_data():
+    with open("nice_data.json", "w+") as data_file:
+        data_file.write(json.dumps(data_file))
+
+def get_top_users():
+    cnt = 0
+    return_value = ""
+    tmp = sorted(data, key=lambda x: x[1], reverse=True)
+    for key in tmp:
+        if cnt > 5:
+            break
+        else:
+            cnt += 1
+            return_value += key + ": " + tmp[key] + "\n"
+    return return_value
+
+import atexit
+atexit.register(save_data())
